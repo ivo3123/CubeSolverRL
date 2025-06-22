7695325eaa1820f9a7969341c73176acd039267f
hrisi14 <hristinafrangova.hf@gmail.com>
2025-06-22 12:54:21 +0300

    Added a 3D cube corresponding to the Speffz lettering backend represenation

---
+++ frontend/src/components/Controls.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="controls">
+    <button @click="$emit('scramble')">Разбъркай</button>
+    <button @click="$emit('step')"> Нова стъпка (RL)</button>
+  </div>
+</template>
+
+<style scoped>
+.controls {
+  padding: 1rem;
+  display: flex;
+  gap: 1rem;
+}
+</style>
